d37912cc9a8071400ce1446cff10af87f5799410
Tim Oleson <Onemanwolf@users.noreply.github.com>
2024-01-08 15:59:24 -0500

    added code

---
+++ commands.ps1
@@ -0,0 +1,12 @@
+az group create --name rg-web-app-avaya-bot --location eastus
+
+az acr create --resource-group rg-web-app-avaya-bot --name webappavaya --sku Basic --admin-enabled true
+
+$ACR_PASSWORD=$(az acr credential show --resource-group rg-web-app-avaya-bot --name webappavaya --query "passwords[?name == 'password'].value" --output tsv)
+
+az acr build --resource-group rg-web-app-avaya-bot --registry webappavaya  --image avayabot:v1 .
+
+az appservice plan create --name webappavayaplan --resource-group rg-web-app-avaya-bot --sku B1 --is-linux
+
+az webapp create --resource-group rg-web-app-avaya-bot --plan webappavayaplan --name avayabotv1 --docker-registry-server-password $ACR_PASSWORD --docker-registry-server-user webappavaya --role acrpull --deployment-container-image-name webappavaya.azurecr.io/avayabot:v1
+
---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = "6bb6245f-400c-4e31-9473-e6dd60c96023"
+    APP_PASSWORD = ".hQ8Q~QOw8VZLRCgYkgHXkzMt2u_VhAA5Lxdic4W" #os.environ.get("MICROSOFT_APP_PASSWORD", "")
